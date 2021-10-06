14e1c875572246c349cd7478d6ea3c588232ff2d
ssilvidi <steve.silvidi@rackspace.com>
2021-10-06 09:31:08 -0500

    initial commit

---
+++ imgmgr.tf/main.tf
@@ -0,0 +1,202 @@
+terraform {
+  backend "s3" {
+    key = "imgmgr.tfstate"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Application = var.imgmgr
+    }
+  }
+}
+
+# data "terraform_remote_state" "vpc" {
+#   backend = "s3"
+#   config = {
+#     bucket = "ssilvidi-dev-tf-state-terraformstatebucket-1my31yzv88c0f"
+#     region = "us-east-2"
+#     key = "vpc.tfstate"
+#    }
+# }
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  role = aws_iam_role.imgmgr_role.name
+}
+
+resource "aws_iam_role" "imgmgr_role" {
+  name = "imgmgr_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"]
+
+  inline_policy {
+    name = "s3getputdelete"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:DeleteObject"
+          ]
+          Effect = "Allow"
+          Resource = "${aws_s3_bucket.img_bucket.arn}/*"
+        }
+      ]
+    })
+  }
+
+  inline_policy {
+    name = "s3listbucket"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = ["s3:ListBucket"]
+          Effect = "Allow"
+          Resource = aws_s3_bucket.img_bucket.arn
+        }
+      ]
+    })
+  }
+
+  inline_policy {
+    name = "ec2desctags"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = ["ec2:DescribeTags"]
+          Effect = "Allow"
+          Resource = "*"
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_s3_bucket" "img_bucket" {
+  bucket_prefix = "${var.customer}-${var.environment}-"
+}
+
+resource "aws_lb" "load_balancer" {
+  security_groups = [aws_security_group.sg_lb.id]
+  subnets = var.load_balancer_subnets
+  tags = {
+    Environment = var.environment
+    Application = var.application
+  }
+}
+
+resource "aws_alb_target_group" "lb_targets" {
+  port = 80
+  protocol = "HTTP"
+  # vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id = var.vpc_id
+}
+
+resource "aws_alb_listener" "lb_http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.lb_targets.arn
+  }
+}
+
+resource "aws_security_group" "sg_lb" {
+  description = "allow http from internet"
+  # vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id = var.vpc_id
+
+  ingress {
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "sg_server" {
+  description = "allow http to app servers"
+  # vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id = var.vpc_id
+
+  ingress {
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      security_groups  = [aws_security_group.sg_lb.id]
+  }
+
+  egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_launch_template" "imgmgr_template" {
+  name = "imgmgr_template"
+  image_id = var.image_id
+  instance_type = var.instance_type
+  key_name = var.ssh_key
+  vpc_security_group_ids = [aws_security_group.sg_server.id]
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.instance_profile.arn
+  }
+
+  user_data = base64encode(templatefile("${path.module}/install.sh", { S3Bucket = aws_s3_bucket.img_bucket.id }))
+}
+
+resource "aws_autoscaling_group" "app_server_group" {
+  name_prefix = "${var.customer}-${var.environment}-"
+  max_size = var.app_server_max_count
+  min_size = var.app_server_min_count
+  vpc_zone_identifier = var.app_subnets
+  health_check_grace_period = 300
+  health_check_type = var.health_check_type
+  target_group_arns = [aws_alb_target_group.lb_targets.arn]
+
+  launch_template {
+    id = aws_launch_template.imgmgr_template.id
+    version = "$Latest"
+  }
+}
---
+++ vpc.tf/vpc.tf
@@ -0,0 +1,168 @@
+terraform {
+  backend "s3" {
+    key = "vpc.tfstate"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+resource "aws_vpc" "TFtopVPC" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.customer}-${var.environment} VPC"
+  }
+}
+
+resource "aws_internet_gateway" "TFtopIGW" {
+  vpc_id = aws_vpc.TFtopVPC.id
+  tags = {
+    Name = "${var.customer}-${var.environment} IGW"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "TFtopSubnetPub1" {
+  vpc_id                  = aws_vpc.TFtopVPC.id
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = "10.220.0.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.customer}-${var.environment} Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "TFtopSubnetPub2" {
+  vpc_id                  = aws_vpc.TFtopVPC.id
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  cidr_block              = "10.220.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.customer}-${var.environment} Public Subnet 2"
+  }
+}
+
+resource "aws_subnet" "TFtopSubnetPriv1" {
+  vpc_id                  = aws_vpc.TFtopVPC.id
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = "10.220.3.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.customer}-${var.environment} Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "TFtopSubnetPriv2" {
+  vpc_id                  = aws_vpc.TFtopVPC.id
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  cidr_block              = "10.220.4.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.customer}-${var.environment} Private Subnet 2"
+  }
+}
+
+resource "aws_eip" "TFtopNatEIPs" {
+  count = 2
+}
+
+resource "aws_nat_gateway" "TFtopNat1" {
+  subnet_id     = aws_subnet.TFtopSubnetPub1.id
+  allocation_id = aws_eip.TFtopNatEIPs[0].id
+  depends_on    = [aws_internet_gateway.TFtopIGW]
+  tags = {
+    Name = "${var.customer}-${var.environment} NAT Gateway 1"
+  }
+}
+
+resource "aws_nat_gateway" "TFtopNat2" {
+  subnet_id     = aws_subnet.TFtopSubnetPub2.id
+  allocation_id = aws_eip.TFtopNatEIPs[1].id
+  depends_on    = [aws_internet_gateway.TFtopIGW]
+  tags = {
+    Name = "${var.customer}-${var.environment} NAT Gateway 2"
+  }
+}
+
+resource "aws_route_table" "TFtopPubRT" {
+  vpc_id     = aws_vpc.TFtopVPC.id
+  depends_on = [aws_internet_gateway.TFtopIGW]
+  route {
+    gateway_id = aws_internet_gateway.TFtopIGW.id
+    cidr_block = "0.0.0.0/0"
+  }
+  tags = {
+    Name = "${var.customer}-${var.environment} Public Route Table"
+  }
+}
+
+resource "aws_route_table" "TFtopPrivRT1" {
+  vpc_id     = aws_vpc.TFtopVPC.id
+  depends_on = [aws_nat_gateway.TFtopNat1]
+  route {
+    nat_gateway_id = aws_nat_gateway.TFtopNat1.id
+    cidr_block     = "0.0.0.0/0"
+  }
+  tags = {
+    Name = "${var.customer}-${var.environment} Private Route Table 1"
+  }
+}
+
+resource "aws_route_table" "TFtopPrivRT2" {
+  vpc_id     = aws_vpc.TFtopVPC.id
+  depends_on = [aws_nat_gateway.TFtopNat2]
+  route {
+    nat_gateway_id = aws_nat_gateway.TFtopNat2.id
+    cidr_block     = "0.0.0.0/0"
+  }
+  tags = {
+    Name = "${var.customer}-${var.environment} Private Route Table 2"
+  }
+}
+
+resource "aws_route_table_association" "TFtopPub1RTAssociation" {
+  subnet_id      = aws_subnet.TFtopSubnetPub1.id
+  route_table_id = aws_route_table.TFtopPubRT.id
+}
+
+resource "aws_route_table_association" "TFtopPub2RTAssociation" {
+  subnet_id      = aws_subnet.TFtopSubnetPub2.id
+  route_table_id = aws_route_table.TFtopPubRT.id
+}
+
+resource "aws_route_table_association" "TFtopPriv1RTAssociation" {
+  subnet_id      = aws_subnet.TFtopSubnetPriv1.id
+  route_table_id = aws_route_table.TFtopPrivRT1.id
+}
+
+resource "aws_route_table_association" "TFtopPriv2RTAssociation" {
+  subnet_id      = aws_subnet.TFtopSubnetPriv2.id
+  route_table_id = aws_route_table.TFtopPrivRT2.id
+}
+
+# outputs
+
+output "vpc_id" {
+  value = aws_vpc.TFtopVPC.id
+}
+
+output "availability_zones" {
+  value = "${data.aws_availability_zones.available.names[0]}"
+}
+
+output "name" {
+  
+}

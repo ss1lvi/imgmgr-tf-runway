71694d836cf63bed698459bdb9a094e2eaeec237
ssilvidi <steve.silvidi@rackspace.com>
2021-10-06 11:38:07 -0500

    added cloudfront

--- imgmgr.tf/main.tf
+++ imgmgr.tf/main.tf
@@ -269,11 +269,48 @@ resource "aws_cloudwatch_metric_alarm" "asg_cpu_low" {
   alarm_actions = [aws_autoscaling_policy.scale_in_policy.arn]
 }
 
+resource "aws_cloudfront_distribution" "cf" {
+  enabled = true
+  price_class = "PriceClass_100"
+  
+  origin {
+    domain_name = aws_lb.load_balancer.dns_name
+    origin_id = "imgmgr-origin"
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+  
+  default_cache_behavior {
+    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "imgmgr-origin"
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
 
 # outputs
 
-output "imgmgr_url" {
+output "cloudfront_url" {
+  value = join("",["https://", aws_cloudfront_distribution.cf.domain_name])
+  description = "The CloudFront URL for img-mgr"
+}
+
+output "lb_url" {
   value = join("",["http://",aws_lb.load_balancer.dns_name])
-  description = "The URL for img-mgr"
+  description = "The Load Balancer URL for img-mgr"
 }

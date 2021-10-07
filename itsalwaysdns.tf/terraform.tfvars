c81a8f1170da888238c704368ffd71fa9d544397
ssilvidi <steve.silvidi@rackspace.com>
2021-10-07 10:42:25 -0500

    fixed a few issues, now it works

--- itsalwaysdns.tf/terraform.tfvars
+++ itsalwaysdns.tf/terraform.tfvars
@@ -1,1 +1,3 @@
+application = "imgmgr"
+environment = "dev"
 hosted_zone = "aws.silvidi.xyz"
--- itsalwaysdns.tf/variables.tf
+++ itsalwaysdns.tf/variables.tf
@@ -1,3 +1,19 @@
+variable "aws_region" {
+  type        = string
+  description = "the AWS region to use"
+  default     = "us-east-1"
+}
+
+variable "application" {
+  type = string
+  description = "name of the application"
+}
+
+variable "environment" {
+  type        = string
+  description = "the environment (prod, dev, etc.)"
+}
+
 variable "hosted_zone" {
   type = string
   description = "your hosted zone in route 53"
